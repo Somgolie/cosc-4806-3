a297e652213751e28f5247d88022eecc450cad28
Somgolie <sejinkeonye@algomau.ca>
2025-06-20 21:18:07 +0000

    added method index to signup for messages

--- app/controllers/signup.php
+++ app/controllers/signup.php
@@ -1,15 +1,11 @@
 <?php
 
 class Signup extends Controller {
-    public function index() {
-        $message = '';
-        $user = $this->model('User');
-        $data = $user->test();
 
-        $this->view('Signup/index', [
-            'message' => $message,
-            'data' => $data
-        ]);
-        die;
+    public function index(){
+        $message = $_SESSION['signup_message'] ?? '';
+        unset($_SESSION['signup_message']); // clear message after showing
+
+        $this->view('Signup/index', ['message' => $message]);
     }
 }
